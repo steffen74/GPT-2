26eb396dbdd11348fcba6473425cc94dfac078e5
bergsteve <57681717+bergsteve@users.noreply.github.com>
2021-01-18 15:54:53 +0100

    Update clustering.py
    I added a keyword list with keywords related to management( 373 keywords)

--- clustering.py
+++ clustering.py
@@ -20,17 +20,21 @@ abstracts = []
 keywords = []
 ## setting up the list of keywords to filter out the abstracts related to climate change
 
-
-keyword_list = ['Sustainability', 'sustainability','sustainable development','Sustainable development','Sustainable Development',
+# 193 abstracts with this keyword list
+keyword_list_climate = ['Sustainability', 'sustainability','sustainable development','Sustainable development','Sustainable Development',
                 'globalization', 'Globalization',
                 'environment', 'Environment'
                 'climate change', 'Climate change', 'Climate Change',
                 'energy','Energy', 'sustainable development goals' ]
+# (116 + 88 + 55 + 43 + 36 + 35 = 373 ) keywords related to management topics
+keyword_list_management = ['innovation', 'Innovation', 'entrepreneurship','Entrepreneurship', 'human capital', 'Human capital','Productivity','productivity',
+                  'performance','Performance', 'efficiency','Efficiency']
 
 #count = 100
 #j = 0
 # here we filter only the abstracts which fit a specific keyword
-for item in keyword_list:
+# change keyword_list_climate to a different keyword_list
+for item in keyword_list_climate:
     for i in os.listdir():
 
         with open(i, "r", encoding='utf-8', errors='ignore') as f:
