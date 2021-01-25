b41c1b552995a041e274dabbac13301a9b6988e5
LeoVerto <LeoVerto@users.noreply.github.com>
2021-01-25 15:43:56 +0100

    Ignore short lines when merging fulltext papers

--- 02a_merge_fulltexts.py
+++ 02a_merge_fulltexts.py
@@ -10,8 +10,11 @@ with open("full_texts_merged.txt", "w") as out_file:
   for f_name in os.listdir(dir):
     path = os.path.join(dir, f_name)
     with open(path, "r") as f:
-      f_text = ''.join(f.readlines()) # Fulltext as a single string
-      f_text = re.sub(r"\n[^\n]", "", f_text) # Remove Carriage Return due to Scanning (i.e. only if no carriage return follows immediatly)
-      #papers.append(f_text)
+      lines = []
+      for line in f:
+        if len(line) > 20: # Remove short lines that are likely result of tables
+          lines.append(line)
+      f_text = "".join(lines) # Fulltext as a single string
+      f_text = re.sub(r"\n(?=\n)", "", f_text)
       out_file.write(f_text)
       out_file.write("\n")
