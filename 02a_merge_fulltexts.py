bf371f4f2fbfb726634265e83d54b0b48f773287
LeoVerto <LeoVerto@users.noreply.github.com>
2021-01-25 16:10:36 +0100

    Fix carriage return handling, remove empty lines

--- 02a_merge_fulltexts.py
+++ 02a_merge_fulltexts.py
@@ -15,6 +15,8 @@ with open("full_texts_merged.txt", "w") as out_file:
         if len(line) > 20: # Remove short lines that are likely result of tables
           lines.append(line)
       f_text = "".join(lines) # Fulltext as a single string
-      f_text = re.sub(r"\n(?=\n)", "", f_text)
+      f_text = re.sub(r"\n(?!\n)", " ", f_text) # Remove Carriage Return due to Scanning (i.e. only if no carriage return follows immediatly)
+      f_text = re.sub(r"\n+(?=\n)", "", f_text) # Remove empty lines
+      #papers.append(f_text)
       out_file.write(f_text)
       out_file.write("\n")
